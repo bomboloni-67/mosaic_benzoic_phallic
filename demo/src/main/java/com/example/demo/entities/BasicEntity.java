5340852db26a909a508bbb2e8cb5238643d9c59a
Jayden <babyzdriever@example.com>
2026-01-21 12:02:46 +0800

    Initial push of library management system project

---
+++ demo/src/main/java/com/example/demo/entities/BasicEntity.java
@@ -0,0 +1,23 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+
+@MappedSuperclass
+public abstract class BasicEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ demo/src/main/java/com/example/demo/entities/Book.java
@@ -0,0 +1,33 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "books")
+public class Book extends BasicEntity {
+    private String title;
+    private String author;
+    private String isbn;
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }   
+    public String getIsbn() {
+        return isbn;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setTitle(String title) {this.title = title;}
+    public void setAuthor(String author) {this.author = author;}
+    public void setIsbn(String isbn) {this.isbn = isbn;}
+    public void setDescription(String description) {this.description = description;}
+
+}
